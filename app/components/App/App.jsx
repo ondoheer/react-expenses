37022eb164f3a38566aefb84533852ef64e082d4
ondoheer <ondoheer@gmail.com>
2018-02-21 12:02:59 -0500

    changed back to hand picked routes, have to refactor that

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -3,14 +3,13 @@ import { connect } from "react-redux";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import history from "../../index";
 
-import {
-  EnhancedCategoryCreateRoute,
-  EnhancedExpenseCreateRoute,
-  EnhancedExpensesRoute,
-  EnhancedHomeRoute,
-  EnhancedLoginRoute,
-  EnhancedRegisterRoute
-} from "../routes";
+import HomeContainer from "../../containers/HomeContainer";
+import LoginContainer from "../../containers/LoginContainer";
+import RegisterContainer from "../../containers/RegisterContainer";
+import AddExpenseContainer from "../../containers/AddExpenseContainer";
+import AddCategoryContainer from "../../containers/AddCategoryContainer";
+import ExpensesContainer from "../../containers/ExpensesContainer";
+
 import NotFoundContainer from "../../containers/NotFoundContainer";
 
 const mapStateToProps = state => ({});
@@ -20,12 +19,60 @@ const App = props => {
     <Router history={history}>
       <div className="container">
         <Switch>
-          <EnhancedLoginRoute exact path="/login" />
-          <EnhancedRegisterRoute exact path="/register" />
-          <EnhancedHomeRoute exact path="/main" />
-          <EnhancedExpenseCreateRoute exact path="/expense/create" />
-          <EnhancedExpensesRoute exact path="/expense" />
-          <EnhancedCategoryCreateRoute exact path="/category/create" />
+          <Route
+            path="/login"
+            render={() => {
+              const isLoged = localStorage.getItem("access_token");
+              return isLoged ? <Redirect to="/" /> : <LoginContainer />;
+            }}
+          />
+          <Route
+            path="/register"
+            render={() => {
+              const isLoged = localStorage.getItem("access_token");
+              return isLoged ? <Redirect to="/" /> : <RegisterContainer />;
+            }}
+          />
+
+          <Route
+            exact
+            path="/main"
+            render={() => {
+              const isLoged = localStorage.getItem("access_token");
+              return isLoged ? <HomeContainer /> : <Redirect to="/login" />;
+            }}
+          />
+          <Route
+            path="/expense/create"
+            render={() => {
+              const isLoged = localStorage.getItem("access_token");
+              return isLoged ? (
+                <AddExpenseContainer />
+              ) : (
+                <Redirect to="/login" />
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/expense"
+            render={() => {
+              const isLoged = localStorage.getItem("access_token");
+              return isLoged ? <ExpensesContainer /> : <Redirect to="/login" />;
+            }}
+          />
+          <Route
+            path="/category/create"
+            render={() => {
+              const isLoged = localStorage.getItem("access_token");
+              return isLoged ? (
+                <AddCategoryContainer />
+              ) : (
+                <Redirect to="/login" />
+              );
+            }}
+          />
+
           <Route component={NotFoundContainer} />
         </Switch>
       </div>
